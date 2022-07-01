b5a6d34191d4b91771dd70b6a1c2c022255dea60
TonyAbrunhosa <37698049+TonyAbrunhosa@users.noreply.github.com>
2022-07-01 16:37:47 -0300

    Primeiro Commit, Criação dos projetos necessarios e implementacao de classes para utilidades e cominicacao com o banco de dados

---
+++ DigitalBrasilCash/DigitalBrasilCash.Shared/Utilities/ReturnErrors.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using FluentValidation.Results;
+using System.Collections.Generic;
+
+namespace DigitalBrasilCash.Shared.Utilities
+{
+    public static class ReturnErrors
+    {
+        public static object CreateObjetError(IList<ValidationFailure> errors)
+        {
+            return errors.Select(x => new { x.PropertyName, x.ErrorMessage });
+        }
+    }
+}
