6887c00cf2551ed685f705d4176eaeb8279a1867
TonyAbrunhosa <37698049+TonyAbrunhosa@users.noreply.github.com>
2022-07-01 17:12:03 -0300

    Criado classes de configuracao API

---
+++ DigitalBrasilCash/DigitalBrasilCash.API/Configurations/Compression.cs
@@ -0,0 +1,33 @@
+﻿using System.IO.Compression;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DigitalBrasilCash.API.Configurations
+{
+    public static class Compression
+    {
+        public static void AddCompression(this IServiceCollection services)
+        {
+            services.AddResponseCompression(options =>
+            {
+                options.EnableForHttps = true;
+                options.Providers.Add<GzipCompressionProvider>();
+            });
+            services.Configure<GzipCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.Optimal;
+            });
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+            services.Configure<IISServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+        }
+    }
+}
+
---
+++ DigitalBrasilCash/DigitalBrasilCash.API/Configurations/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using DigitalBrasilCash.Shared.Communication;
+using DigitalBrasilCash.Shared.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DigitalBrasilCash.API.Configurations
+{
+    public static class DependencyInjection
+    {
+        public static void ResolveDependencyInjection(this IServiceCollection services)
+        {
+            services.AddScoped<AppSettings, AppSettings>();
+            services.AddScoped<SqlCommunication, SqlCommunication>();
+
+            ////SERVICES
+            //services.AddTransient<ITokenService, TokenService>();
+
+            ////REPOSITORY
+            //services.AddTransient<ITokenRepository, TokenRepository>();
+
+        }
+    }
+}
+
