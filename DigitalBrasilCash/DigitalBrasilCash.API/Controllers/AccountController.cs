e67b38808f16efc8ef3b72dcc58e7b1c6a3112aa
TonyAbrunhosa <37698049+TonyAbrunhosa@users.noreply.github.com>
2022-07-02 20:06:31 -0300

    implemnetacao controller account metodo novo

---
+++ DigitalBrasilCash/DigitalBrasilCash.API/Controllers/AccountController.cs
@@ -0,0 +1,74 @@
+﻿using DigitalBrasilCash.Application.Services;
+using DigitalBrasilCash.Domain.Accounts.Input;
+using DigitalBrasilCash.Domain.Accounts.Output;
+using DigitalBrasilCash.Domain.Command;
+using DigitalBrasilCash.Domain.Contracts.Command;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace DigitalBrasilCash.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    public class AccountController : Controller
+    {
+        private readonly AccountWriteService _serviceWrite;
+
+        public AccountController(AccountWriteService serviceWrite)
+        {
+            _serviceWrite = serviceWrite;
+        }
+
+        [HttpPost("account")]
+        [ProducesResponseType(typeof(AccountOutput), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ICommandResult), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ICommandResult), StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(typeof(ICommandResult), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Novo([FromBody] AccountInput input)
+        {
+            try
+            {
+                ICommandResult retorno = await _serviceWrite.Novo(input);
+
+                if (retorno.Sucesso)
+                    return Ok(retorno);
+                else if (!retorno.Sucesso && retorno.Dados != null)
+                    return UnprocessableEntity(retorno);
+                else
+                    return BadRequest(retorno);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new CommandResult(false, ex.Message + ex.StackTrace));
+            }
+        }
+
+        [HttpPost("account-local")]
+        [ProducesResponseType(typeof(AccountOutput), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ICommandResult), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ICommandResult), StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(typeof(ICommandResult), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> NovoLocal([FromBody] AccountInput input)
+        {
+            try
+            {
+                ICommandResult retorno = await _serviceWrite.NovoLocal(input);
+
+                if (retorno.Sucesso)
+                    return Ok(retorno);
+                else if (!retorno.Sucesso && retorno.Dados != null)
+                    return UnprocessableEntity(retorno);
+                else
+                    return BadRequest(retorno);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new CommandResult(false, ex.Message + ex.StackTrace));
+            }
+        }
+    }
+}
